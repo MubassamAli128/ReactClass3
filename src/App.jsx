bbf2c419840cd4e89085e0c2d93e927053101a4d
mubassam ali <mubassamali128@gmail.com>
2025-01-13 15:09:50 +0500

    Done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import Time from "./components/Time"
+import TotalAmount from "./components/TotalAmount"
+import Dates from './components/Dates'
+import './index.css'
 function App() {
 
   return (
     <>
-      <div>
-        <Time/>
+      <div className='rounded-2xl bg-gray-200 p-4 flex flex-col gap-3'>
+        <TotalAmount/>
+        <hr className='w-full border-gray-500'/>
+        <div className='flex items-center justify-between'>
+          <Dates/>
+        </div>
       </div>
     </>
   )
---
+++ src/components/Dates.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Dates() {
+  return (
+    <div className='flex items-center justify-between gap-2'>
+        <input type="date" name="" id="" className='bg-transparent'/>
+        <input type="date" name="" id=""  className='bg-transparent'/>
+    </div>
+  )
+}
+
+export default Dates
