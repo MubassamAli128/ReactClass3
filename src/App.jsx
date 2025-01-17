e63d88dfb2709382cc1df2a6660f65abf280f55c
mubassam ali <mubassamali128@gmail.com>
2025-01-17 15:56:22 +0500

    A2 start.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import TotalAmount from "./components/TotalAmount"
-import Dates from './components/Dates'
-import { FaSearch } from "react-icons/fa";
-import DataTable from "./components/DataTable"
-import data from "./data.json";
 import './index.css'
 
 function App() {
