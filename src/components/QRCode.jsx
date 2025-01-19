c93ec3e670f1ecf0e8f0ccf16c8c5db42c93366a
mubassam ali <mubassamali128@gmail.com>
2025-01-19 16:43:23 +0500

    A2 QR

--- src/components/QRCode.jsx
+++ src/components/QRCode.jsx
@@ -4,12 +4,16 @@ import OR from './OR'
 import Button from './Button'
 import { MdOutlineCopyAll } from "react-icons/md";
 import { IoMdHelpCircleOutline } from "react-icons/io";
-import QRImage from "../assets/"
+import Image from "../assets/QR.png"
+import "./style.css"
 
 function QRCode(props) {
 
     const handleQRCode=(event)=>{
-
+        if(event.target.id=="Copy"){
+            navigator.clipboard.writeText("https://mubassamali128.github.io/My_Portfolio/");
+            props.onClick("Copy Done!","success")
+        }
     }
 
 
@@ -17,8 +21,17 @@ function QRCode(props) {
     <div className='w-[500px] bg-[#f9f8fe] rounded-2xl py-10 px-4'>
         <Title text="2FA setup"/>
         <p className='text-center text-gray-400'>Scan the image below with your 2FA authenticator.</p>
-        <div>
-            <img src="./" alt="" />
+        <div className='relative w-full flex flex-col items-center justify-center mt-2 overflow-hidden h-[180px]'>
+            <div className=' p-1 relative'>
+                <div className='absolute w-8 h-8 border-2 border-t-[#7b35fb] border-l-[#7b35fb] border-b-transparent border-r-transparent top-0 left-0'></div>
+                <div className='absolute w-8 h-8 border-2 border-t-[#7b35fb] border-r-[#7b35fb] border-b-transparent border-l-transparent top-0 right-0'></div>
+                <div className='absolute w-8 h-8 border-2 border-l-[#7b35fb] border-b-[#7b35fb] border-r-transparent border-t-transparent bottom-0 left-0'></div>
+                <div className='absolute w-8 h-8 border-2 border-r-[#7b35fb] border-b-[#7b35fb] border-l-transparent border-t-transparent bottom-0 right-0'></div>
+                <img src={Image} className='w-[150px]' alt="" />
+            </div>
+            <div className='animation absolute -bottom-1 border-2 border-transparent border-r-transparent border-l-transparent border-t-[#7b35fb] w-[50%] flex justify-center'>
+                <div className='bg-gradient-to-b from-[#7b35fb] to-transparent p-4 w-[90%] h-full'></div>
+            </div>
         </div>
         <OR/>
         <p className='text-center text-gray-400'>Manually enter the code below.</p>
